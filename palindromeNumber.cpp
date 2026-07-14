9423762f748cc671d6f223fd16f68f01722113ef
aarusharyal <aryalaarush203@gmail.com>
2026-07-14 18:25:29 +0545

    Added Necessary Comments

--- palindromeNumber.cpp
+++ palindromeNumber.cpp
@@ -4,15 +4,18 @@ int main()
 {
 	int num,rev=0,original,rem;
 	cout<<"Enter the number : ";
+	// Asking the user for the number
 	cin>>num;
 	original= num;
 	while(num!=0)
 	{
+		// Reversing the number
 		rem=num%10;
 		rev=rev*10+rem;
 		num=num/10;
 	}
 	if(original==rev)
+	// Checking if the number is palnindrome or not
 	cout<<"It is a Palindrome Number";
 	
 	else
