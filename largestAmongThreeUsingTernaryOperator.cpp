61cf502b9bc24bc44a5db3dc3e39ef3e13058a31
aarusharyal <aryalaarush203@gmail.com>
2026-07-16 20:59:22 +0545

    Added Comments

--- largestAmongThreeUsingTernaryOperator.cpp
+++ largestAmongThreeUsingTernaryOperator.cpp
@@ -6,6 +6,7 @@ int main()
     int a,b,c,largest;
     cout<<"Enter three numbers: ";
     cin>>a>>b>>c;
+    // Usage of ternary operator
     largest = (a>b) ? ((a>c) ? a : c) : ((b>c) ? b : c);
     cout<<"Largest number = "<<largest;
     return 0;
