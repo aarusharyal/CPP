c493a9c9f76b2ca0562ffc787528b7a7d03a09bc
aarusharyal <aryalaarush203@gmail.com>
2026-07-16 21:00:07 +0545

    Added Comments

--- rightAngledStarPattern.cpp
+++ rightAngledStarPattern.cpp
@@ -3,11 +3,13 @@ using namespace std;
 int main()
 {
 	int i,j;
+	// Loop to print the pattern
 	for(i=1;i<=5;i++)
 	{
 		for(j=1;j<=i;j++)
 		{
-			cout<<" *";//to print stars
+			cout<<" *";
+			//to print stars
 		}
 		cout<<endl;
 	}
