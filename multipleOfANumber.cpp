294cdcf3b6675f44bfec83e176ec33e24ca6a032
aarusharyal <aryalaarush203@gmail.com>
2026-07-17 21:23:02 +0545

    Added Comments

--- multipleOfANumber.cpp
+++ multipleOfANumber.cpp
@@ -5,6 +5,7 @@ int main()
 	int num,i;
 	cout<<"Enter the number : ";
 	cin>>num;
+	// Logic for Printing the multiple of numbers
 	for(i=1;i<=10;i++)
 	{
 	cout<<"The multiple of"<<num<<"x"<<i<<"is"<<num*i<<endl;
