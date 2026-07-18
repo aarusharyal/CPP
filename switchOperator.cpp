98f6eb0574bdd7f9d3ae875914dcaa5d4cb978a0
aarusharyal <aryalaarush203@gmail.com>
2026-07-18 20:52:11 +0545

    Switch Operator

--- switchOperator.cpp
+++ switchOperator.cpp
@@ -1,40 +1,55 @@
 #include<iostream>
 using namespace std;
+
 int main()
 {
-	int num1,num2;
+	// Declare two integer operands
+	int num1, num2;
+	// Variable to hold the operator character (+, -, *, /)
 	char operator;
-	cout<<"Enter the numbers"<<endl;
-	cin>>num1>>num2;
-	cout<<"Enter the operator"<<endl;
-	cin>>operator;
+
+	// Prompt the user for two numbers
+	cout << "Enter the numbers" << endl;
+	cin >> num1 >> num2;
+
+	// Prompt the user for the operator to apply
+	cout << "Enter the operator" << endl;
+	cin >> operator;
+
+	// Use switch to select the operation based on the operator char
 	switch(operator)
 	{
 		case '+':
-			int sum=num1+num2;
-			cout<<"sum is"<<sum;
+			// Addition
+			int sum = num1 + num2;
+			cout << "sum is " << sum;
 			break;
 		case '-':
-			int difference=num1-num2;
-			cout<<"Difference is"<<difference;
+			// Subtraction
+			int difference = num1 - num2;
+			cout << "Difference is " << difference;
 			break;
 		case '*':
-			int multiple=num1*num2;
-			cout<<"Multiplication is"<<multiple;
+			// Multiplication
+			int multiple = num1 * num2;
+			cout << "Multiplication is " << multiple;
 			break;
 		case '/':
-			if(num2!=0)
+			// Division with zero check
+			if(num2 != 0)
 			{
-				int division=num1/num2;
-				cout<<"Division is"<<division;
+				int division = num1 / num2; // integer division
+				cout << "Division is " << division;
 			}
 			else
 			{
-				cout<<"Division is not possible";
+				cout << "Division is not possible";
 			}
 			break;
 		default:
-			cout<<"Invalid choice";
+			// Operator not recognized
+			cout << "Invalid choice";
 	}
-	return 0;
+
+	return 0; // indicate successful execution
 }
