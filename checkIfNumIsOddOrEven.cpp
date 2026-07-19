8cf3122edfb0323ea60070c1a0bb375fa065d0c2
aarusharyal <aryalaarush203@gmail.com>
2026-07-19 20:24:27 +0545

    Added Comments

--- checkIfNumIsOddOrEven.cpp
+++ checkIfNumIsOddOrEven.cpp
@@ -1,15 +1,19 @@
-#include<iostream>
+#include <iostream> // Include I/O library
 using namespace std;
+
+// Program to check if a given integer is odd or even
 int main()
 {
-	int num;
-	cout<<"Enter the number : ";
-	cin>>num;
-	if(num%2==0)
-	cout<<num<<" is even number"<<endl;
-	
-	else
-	cout<<num<<" is odd number"<<endl;
-	return 0;
-	 
+	int num; // variable to store input number
+
+	cout << "Enter the number : "; // prompt the user
+	cin >> num; // read number from standard input
+
+	// If remainder of division by 2 is zero, number is even
+	if (num % 2 == 0)
+		cout << num << " is even number" << endl;
+	else // otherwise it's odd
+		cout << num << " is odd number" << endl;
+
+	return 0; // indicate successful termination
 }
