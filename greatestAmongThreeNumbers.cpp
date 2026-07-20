734ceecb08435b585983f7c384aa0b8cb7f658a9
aarusharyal <aryalaarush203@gmail.com>
2026-07-20 21:12:01 +0545

    Added Comments

--- greatestAmongThreeNumbers.cpp
+++ greatestAmongThreeNumbers.cpp
@@ -1,21 +1,28 @@
 #include<iostream>
 using namespace std;
+
 int main()
 {
-	int num1,num2,num3;
-	cout<<"Enter the numbers";
-	cin>>num1>>num2>>num3;
-	if(num1>num2 && num1>num3)
+	// Declare three integer variables to store the input numbers
+	int num1, num2, num3;
+
+	// Prompt the user to enter three numbers
+	cout << "Enter the numbers";
+	cin >> num1 >> num2 >> num3;
+
+	// Compare the numbers and print which one is largest
+	if (num1 > num2 && num1 > num3)
 	{
-		cout<<"Num1 is largest";
+		cout << "Num1 is largest";
 	}
-	else if(num2>num1 && num2>num3)
+	else if (num2 > num1 && num2 > num3)
 	{
-		cout<<"Num2 is largest";
+		cout << "Num2 is largest";
 	}
-	else if(num3>num1 && num3>num2)
+	else if (num3 > num1 && num3 > num2)
 	{
-		cout<<"Num3 is largest";
+		cout << "Num3 is largest";
 	}
+
 	return 0;
 }
