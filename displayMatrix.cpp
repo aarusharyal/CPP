08e0156f55ad36f12be752a65cebab4d001b3f95
aarusharyal <aryalaarush203@gmail.com>
2026-07-20 21:11:03 +0545

    Added Comments

--- displayMatrix.cpp
+++ displayMatrix.cpp
@@ -2,18 +2,25 @@
 using namespace std;
 
 int main() {
+    // Declare variables for matrix dimensions
     int row, col;
     cout<<"Enter number of rows: ";
     cin>>row;
     cout<<"Enter number of columns: ";
     cin>>col;
+    
+    // Create a 2D matrix with given dimensions
     int matrix[row][col];
     cout<<"Enter matrix elements:\n";
+    
+    // Read matrix elements from user
     for(int i=0; i<row; i++) {
         for(int j=0; j<col; j++) {
             cin>>matrix[i][j];
         }
     }
+    
+    // Display the matrix
     cout<<"Matrix:\n";
     for(int i=0; i<row; i++) {
         for(int j=0; j<col; j++) {
